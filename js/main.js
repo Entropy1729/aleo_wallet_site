57b8887bbf7a6237139d085a83b0a4f41cd7c158
AngieDutra <angiedutraa@gmail.com>
2022-10-14 12:04:30 -0500

    landing

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const myAtropos = Atropos({
+    el: ".my-atropos",
+    activeOffset: 40,
+    shadowScale: 1.05,
+  });
+});
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "aleo-dark": "var(--aleo-dark)",
+        "aleo-no-dark": "var(--aleo-no-dark)",
+        "aleo-border": "var(--aleo-border)",
+        "aleo-light": "var(--aleo-light)",
+        "aleo-gray": "var(--aleo-gray)",
+        "aleo-blue": "var(--aleo-blue)",
+        "aleo-green": "var(--aleo-green)",
+        "aleo-light": "var(--aleo-light)",
+        "aleo-lightblue": "var(--aleo-lightblue)",
+        "aleo-transparent": "var(--aleo-transparent)",
+        "aleo-transparent-green": "var(--aleo-transparent-green)",
+        "aleo-transparent-blue": "var(--aleo-transparent-blue)",
+        "aleo-transparent-lightblue": "var(--aleo-transparent-lightblue)",
+      },
+    },
+  },
+  plugins: [],
+};
