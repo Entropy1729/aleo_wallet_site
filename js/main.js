e26657d87d86dc17f20b3339c542bb30ec4fde91
AngieDutra <angiedutraa@gmail.com>
2022-10-17 15:35:24 -0500

    responsive

--- js/main.js
+++ js/main.js
@@ -4,4 +4,21 @@ document.addEventListener("DOMContentLoaded", () => {
     activeOffset: 40,
     shadowScale: 1.05,
   });
+  const browser = () => {
+    const button = document.querySelector(".download-btn");
+    if ((navigator.userAgent.indexOf("Opera") || navigator.userAgent.indexOf("OPR")) != -1) {
+      button.innerHTML = "Download for Opera";
+    } else if (navigator.userAgent.indexOf("Edg") != -1) {
+      button.innerHTML = "Download for Edge";
+    } else if (navigator.userAgent.indexOf("Chrome") != -1) {
+      button.innerHTML = "Download for Chrome";
+    } /*  else if (navigator.userAgent.indexOf("Safari") != -1) {
+      button.innerHTML = "Download for Safari";
+    }  */ else if (navigator.userAgent.indexOf("Firefox") != -1) {
+      button.innerHTML = "Download for Firefox";
+    } else {
+      button.innerHTML = "Try our wallet!";
+    }
+  };
+  browser();
 });
