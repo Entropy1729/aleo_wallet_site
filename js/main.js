3376ce26dabd87d7fdefe9cda66c8ee165a13522
AngieDutra <angiedutraa@gmail.com>
2022-10-19 16:12:45 -0300

    coming soon

--- js/main.js
+++ js/main.js
@@ -4,6 +4,16 @@ document.addEventListener("DOMContentLoaded", () => {
     activeOffset: 40,
     shadowScale: 1.05,
   });
+  const menu = () => {
+    const trigger = document.querySelector(".menu-trigger");
+    const menu = document.querySelector(".nav-options");
+    trigger.addEventListener("click", () => {
+      menu.classList.toggle("show-menu");
+      menu.classList.toggle("hide-menu");
+      document.body.classList.toggle("overflow");
+    });
+  };
+  menu();
   const browser = () => {
     const button = document.querySelector(".download-btn");
     if ((navigator.userAgent.indexOf("Opera") || navigator.userAgent.indexOf("OPR")) != -1) {
@@ -12,13 +22,26 @@ document.addEventListener("DOMContentLoaded", () => {
       button.innerHTML = "Download for Edge";
     } else if (navigator.userAgent.indexOf("Chrome") != -1) {
       button.innerHTML = "Download for Chrome";
-    } /*  else if (navigator.userAgent.indexOf("Safari") != -1) {
+    } else if (navigator.userAgent.indexOf("Safari") != -1) {
       button.innerHTML = "Download for Safari";
-    }  */ else if (navigator.userAgent.indexOf("Firefox") != -1) {
+    } else if (navigator.userAgent.indexOf("Firefox") != -1) {
       button.innerHTML = "Download for Firefox";
     } else {
       button.innerHTML = "Try our wallet!";
     }
   };
   browser();
+  const preview = () => {
+    const features = document.querySelectorAll(".feature");
+    features.forEach((feature) => {
+      feature.addEventListener("mouseenter", () => {
+        feature.querySelector("video").classList.remove("opacity-0");
+        feature.querySelector("video").style.opacity = "1";
+      });
+      feature.addEventListener("mouseleave", () => {
+        feature.querySelector("video").style.opacity = "0";
+      });
+    });
+  };
+  preview();
 });
